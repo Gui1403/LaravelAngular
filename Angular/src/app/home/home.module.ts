90e6a439eed3d04d8008c192dfe6164accb4a7df
Gui1403 <guimilaomoraes@gmail.com>
2026-08-12 12:16:09 -0300

    primeiro commit

---
+++ Angular/src/app/home/home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+
+import { HomePageRoutingModule } from './home-routing.module';
+import { IncrementerModule } from '../shared/components/incrementer/incrementer.module';
+import { MenuModule } from '../shared/components/menu/menu.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule,
+    IncrementerModule,
+    MenuModule
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
