8c965e1f46b9386ccd5ef6743ea9affeb619fed8
Gui1403 <guimilaomoraes@gmail.com>
2026-08-19 12:18:46 -0300

    12:18

--- laravel/app/Models/Aluno.php
+++ laravel/app/Models/Aluno.php
@@ -5,9 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ComponenteModel extends Model
+class Aluno extends Model
 {
     use HasFactory;
-    protected $table = 'componente';
-    protected $fillable = ['nome', 'hora_inicio', 'hora_fim'];
+
+    protected $tables = ['alunos'];
+    protected $fillable = ['nome', 'email'];
 }
